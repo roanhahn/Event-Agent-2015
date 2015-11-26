c8d980c132bfd595aa0561fa00e5844dc9c888e7
Roan Hahn <roan@digitalmonkey.nl>
2015-11-26 17:49:47 +0100

    Fubar

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+ 'use strict';
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -18,150 +18,150 @@
 
 module.exports = function(grunt) {
 
-  require('time-grunt')(grunt);
+	require('time-grunt')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-    config: {
-      src: 'src',
-      dist: 'dist'
-    },
+		config: {
+			src: 'src',
+			dist: 'dist'
+		},
 
-    watch: {
-      assemble: {
-        files: ['<%= config.src %>/{content,data,templates}/{,**/}*.{md,hbs,yml}'],
-        tasks: ['assemble'],
-      },
-      cssmin: {
-          files: [
-              '<%= config.src %>/static/css/styles.css'
-          ],
-          tasks: ['cssmin']
-      },
-      scripts: {
-          files: [
-              '<%= config.src %>/static/css/**/*.scss'
-          ],
-          tasks: ['sass']
-      },
-      concat: {
-        files: [
-          '<%= config.src %>/static/js/*.js'
-        ],
-        tasks: ['concat']
-      },
+		watch: {
+			assemble: {
+			files: ['<%= config.src %>/{content,data,templates}/{,**/}*.{md,hbs,yml}'],
+			tasks: ['assemble'],
+		},
+		cssmin: {
+			files: [
+			'<%= config.src %>/static/css/styles.css'
+			],
+			tasks: ['cssmin']
+		},
+		scripts: {
+			files: [
+			'<%= config.src %>/static/css/**/*.scss'
+			],
+			tasks: ['sass']
+		},
+		concat: {
+			files: [
+			'<%= config.src %>/static/js/*.js'
+			],
+			tasks: ['concat']
+		},
 
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        },
-        files: [
-          '<%= config.dist %>/{,*/}*.html',
-          '<%= config.dist %>/assets/{,*/}*.css',
-          '<%= config.dist %>/assets/{,*/}*.js',
-          '<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
-    },
-
-    
-    connect: {
-      options: {
-        port: 9000,
-        livereload: 35729,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          open: true,
-          base: [
-            '<%= config.dist %>'
-          ]
-        }
-      }
-    },
-
-    concat: {
-      js: {
-        src: '<%= config.src %>/static/js/*.js',
-        dest: '<%= config.dist %>/assets/js/app.js'
-      }
-    },
-
-    sass: {
-      dist: {
-          files: {
-              '<%= config.src %>/static/css/styles.css': '<%= config.src %>/static/css/base.scss'
-          }
-      }
-    },
-   
-    cssmin: {
-      my_target: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.src %>/static/css/',
-          src: ['styles.css'],
-          dest: '<%= config.dist %>/assets/css/',
-          ext: '.min.css'
-        }]
-      }
-    },
+		livereload: {
+			options: {
+				livereload: '<%= connect.options.livereload %>'
+			},
+			files: [
+		'<%= config.dist %>/{,*/}*.html',
+	'<%= config.dist %>/assets/{,*/}*.css',
+'<%= config.dist %>/assets/{,*/}*.js',
+'<%= config.dist %>/assets/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+]
+}
+},
 
 
-    'gh-pages': {
-      options: {
-        base: 'dist'
-      },
-      src: ['**']
-    },
+connect: {
+	options: {
+		port: 9000,
+		livereload: 35729,
+				// change this to '0.0.0.0' to access the server from outside
+				hostname: 'localhost'
+			},
+			livereload: {
+				options: {
+					open: true,
+					base: [
+					'<%= config.dist %>'
+					]
+				}
+			}
+		},
 
-    assemble: {
-      pages: {
-        options: {
-          flatten: true,
-          assets: '<%= config.dist %>/assets',
-          layoutdir: '<%= config.src %>/templates/layouts',
-          layout: 'default.hbs',
-          data: '<%= config.src %>/data/*.{json,yml}',
-          partials: '<%= config.src %>/templates/partials/**/*.hbs'
-        },
-        files: {
-          '<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
-        }
-      }
-    },
+		concat: {
+			js: {
+				src: '<%= config.src %>/static/js/*.js',
+				dest: '<%= config.dist %>/assets/js/app.js'
+			}
+		},
 
-    // Before generating any new files,
-    // remove any previously-created files.
-    clean: ['<%= config.dist %>/**/*.{html,xml}']
+		sass: {
+			dist: {
+					files: {
+						'<%= config.src %>/static/css/styles.css': '<%= config.src %>/static/css/base.scss'
+					}
+				}
+			},
 
-  });
-  
-  grunt.loadNpmTasks('assemble');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-gh-pages');
+			cssmin: {
+				my_target: {
+					files: [{
+						expand: true,
+						cwd: '<%= config.src %>/static/css/',
+						src: ['styles.css'],
+						dest: '<%= config.dist %>/assets/css/',
+						ext: '.min.css'
+					}]
+				}
+			},
 
-  grunt.registerTask('server', [
-    'assemble',
-    'connect:livereload',
-    'watch'
-  ]);
 
-  grunt.registerTask('build', [
-    'clean',
-    'assemble'
-  ]);
+			'gh-pages': {
+				options: {
+					base: 'dist'
+				},
+				src: ['**']
+			},
 
-  grunt.registerTask('default', [
-    'build'
-  ]);
+			assemble: {
+				pages: {
+					options: {
+						flatten: true,
+						assets: '<%= config.dist %>/assets',
+						layoutdir: '<%= config.src %>/templates/layouts',
+						layout: 'default.hbs',
+						data: '<%= config.src %>/data/*.{json,yml}',
+						partials: '<%= config.src %>/templates/partials/**/*.hbs'
+					},
+					files: {
+						'<%= config.dist %>/': ['<%= config.src %>/templates/pages/*.hbs']
+					}
+				}
+			},
+
+		// Before generating any new files,
+		// remove any previously-created files.
+		clean: ['<%= config.dist %>/**/*.{html,xml}']
+
+	});
+
+grunt.loadNpmTasks('assemble');
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-clean');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-gh-pages');
+
+grunt.registerTask('server', [
+	'assemble',
+	'connect:livereload',
+	'watch'
+	]);
+
+grunt.registerTask('build', [
+	'clean',
+	'assemble'
+	]);
+
+grunt.registerTask('default', [
+	'build'
+	]);
 
 };
