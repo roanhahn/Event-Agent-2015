83b758e9f931f626228038664162431c5fa7155c
Roan Hahn <roan@digitalmonkey.nl>
2017-08-07 14:27:11 +0200

    Updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -2987,6 +2987,22 @@ $(".hotel-item-grid").hover
 //   } 
 
 
+// VIMEO
+
+
+//Select the #embeddedVideo element
+var video = document.getElementById('embeddedVideo');
+
+//Create a new Vimeo.Player object
+var player = new Vimeo.Player(video);
+
+//When the player is ready, set the volume to 0
+player.ready().then(function() {
+    player.setVolume(0);
+});
+
+
+
 // PopOver
 
 $(function () {
